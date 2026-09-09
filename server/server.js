79a6407eb3737eb552586586f8f6710f85955509
Shadan0786 <Shadanashrafi82@gmail.com>
2026-09-09 09:27:38 +0530

    Initial CivicFix project setup

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to CivicFix API",
+  });
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`CivicFix server running on port ${PORT}`);
+});
